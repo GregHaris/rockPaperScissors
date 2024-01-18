317241a9e6aaaed552c356c110ae4f4693f0f155
Dac Haven <d.gregharis@gmail.com>
2024-01-19 00:05:52 +0100

    debug the game logic
    line 11 to 12 - make playerSelection case-insensitve rather than getting userInput and then converting input toLowerCase
    line 16 - assigning the computerSelection parameter to the getComputerChoice() function
    line 40 to 43 - change the game call and test the playRound() function

--- gameScript.js
+++ gameScript.js
@@ -1,37 +1,43 @@
 
 // A function getComputerChoice to randomly return "Rock", "Paper" or "Scissors"
-const options = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(){
+    const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
 }
 
-const randomWord = getComputerChoice();
-
 // A function to play a single round that takes two parameters
 function playRound(playerSelection, computerSelection){
-    let getInput = prompt("what is the word? 'Rock', 'Paper' or Scissors'? ");
-    // get user input, convert and assign input to playerSelection
-    playerSelection = getInput.toLocaleLowerCase();
-    // assign getComputerChoice() as computer selection
-    computerSelection = randomWord.toLowerCase;
-    // if playerSelection not in option, return error 
-    if (!options.includes(playerSelection)) {
+    // Make playSelection Case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+
+    // assign getComputerChoice() as computer selection and also make it case-Insensitive
+    computerSelection = getComputerChoice().toLowerCase();
+
+    // if playerSelection not in "rock", "paper", "scissors", return error 
+    if (!["rock", "paper", "scissors"].includes(playerSelection)) {
         return "Invalid input. word is 'Rock', 'Paper', or 'Scissors'.";
     }
+
     // if playerSelection equals computerSelection its a tie
     if (playerSelection === computerSelection){
-        return "😊 Its a tie! Let's play again"
+        return "😊 Its a tie! Let's play again";
     }
+
     // set the winning criteria and declaring the winner
     if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
       ) {
-        return `You win 🎉! ${playerSelection} beats ${computerSelection}`
+        return `You win 🎉! ${playerSelection} beats ${computerSelection}`;
       } else {
-        return `You lose 🤣! ${computerSelection} beats ${playerSelection}`
+        return `You lose 🤣! ${computerSelection} beats ${playerSelection}`;
       }
 }
+
+// test the playRound()
+const playerSelection = prompt("What is the word?", "Enter 'Rock', 'Paper' or 'Scissor'" );
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
