8ce29fae163fff6724c4cbd10ac2a70000275c6e
Dac Haven <d.gregharis@gmail.com>
2024-02-09 07:03:22 +0100

    modified gameScript file
    The game keeps misbehaving. so I am rewriting the entire code from scratch.Thus far, I have
    line 1 & 2: set the game score variables
    line 4 to 10 selected elements from the index docment and stored eachin their variables
    line 14 to 28: assign event listeners to the game image buttons (rock_div, paper_div and scissors_div) and place them inside a function 'main()'

--- static/js/gameScript.js
+++ static/js/gameScript.js
@@ -1,16 +1,29 @@
 const playerScore= 0;
 const computerScore = 0;
 
-let playerScore_span = document.querySelector(".playerScore");
-let computerScore_span = document.querySelector(".computerScore");
+const playerScore_span = document.querySelector(".playerScore");
+const computerScore_span = document.querySelector(".computerScore");
+const scoreBoard_div = document.querySelector("#scoreBoard");
+const winner = document.querySelector(".winner")
+const rock_div = document.querySelector("#r");
+const paper_div = document.querySelector("#p");
+const scissors_div = document.querySelector("#s");
 
-const scoreBoard_div = document.querySelector("scoreBoard");
-let playerChoice;
 
-const rockBtn = document.querySelector("#rock");
-rockBtn.addEventListener("click", () => {
-  playerChoice = "rock";
-});
 
-const nameDisplay = document.querySelector("#nameDisplay");
-nameDisplay.textContent = `PlayerChoice: ${playerChoice}`
+function main() {
+  rock_div.addEventListener("click", () => {
+    game("r");
+  })
+
+  paper_div.addEventListener("click", () => {
+    game("p");
+  })
+
+  scissors_div.addEventListener("click", () => {
+    game("s");
+  })
+};
+
+main();
+
