c89f03369efebf5600d25f0f5ee0179fb609cf3f
Dac Haven <d.gregharis@gmail.com>
2024-02-10 00:43:21 +0100

    modified js file
    set the game rules for the user to select the number of rounds to play, announce the winner of each round, announce the over all game winner after game over and clears all the announcements on the screen when the player change the number o rounds

--- static/js/gameScript.js
+++ static/js/gameScript.js
@@ -1,44 +1,73 @@
 let playerScore= 0;
 let computerScore = 0;
+let roundsPlayed = 0;
+let selectedGameRound = 0;
 
 const playerScore_span = document.querySelector(".playerScore");
 const computerScore_span = document.querySelector(".computerScore");
 const scoreBoard_div = document.querySelector("#scoreBoard");
-const winner = document.querySelector(".winner")
 const rock_div = document.querySelector("#r");
 const paper_div = document.querySelector("#p");
 const scissors_div = document.querySelector("#s");
-let roundResult = document.querySelector(".roundResult")
+let roundResult = document.querySelector(".roundResult");
+let gameOverMessage_h3 = document.querySelector("#gameOverMessage");
 
+// add event listener when user selects the number of rounds to play
+let gameRoundOptions = document.querySelector("#gameRound");
+gameRoundOptions.addEventListener("change", () => {
+  selectedGameRound = parseInt(gameRoundOptions.value); // stores the int converted value of the option selected
+  roundsPlayed = 0; // resets the roundsPlayed to default
+  gameOverMessage_h3.textContent = ""; // clears the game over message
+  roundResult.textContent = ""; // clears the round result message
+});
 
+gameRoundOptions.focus();
+
+// function to get the computer's choice
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 };
 
+// game criterial
 function game(playerChoice) {
   const computerChoice = getComputerChoice();
 
-  // game rules
-  if (playerChoice === computerChoice) {
-      roundResult.textContent =  "Its a tie 🤝! Let's play again";
-} else if (
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "paper" && computerChoice === "rock") ||
-    (playerChoice === "scissors" && computerChoice === "paper")
-  ) {
-    playerScore++
-    playerScore_span.textContent = playerScore;
-    roundResult.textContent = `You win 🔥! ${playerChoice} beats ${computerChoice}`;
-  } else {
-    computerScore++ 
-    computerScore_span.textContent = computerScore;       
-    roundResult.textContent =  `You lose 🤣! ${computerChoice} beats ${playerChoice}`;
+// game rules
+  if (roundsPlayed < selectedGameRound) {
+    if (playerChoice === computerChoice) {
+      roundResult.textContent =  "Its a tie 🤝!";
+    } else if (
+          (playerChoice === "rock" && computerChoice === "scissors") ||
+          (playerChoice === "paper" && computerChoice === "rock") ||
+          (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      playerScore++
+      playerScore_span.textContent = playerScore;
+      roundResult.textContent = `You win 🔥! ${playerChoice} beats ${computerChoice}`;
+    } else {
+      computerScore++ 
+      computerScore_span.textContent = computerScore;       
+      roundResult.textContent =  `You lose 🤣! ${computerChoice} beats ${playerChoice}`;
+    }
   }
+
+  
+  roundsPlayed++ // increase roundsPlayed after each round.
+
+  if (roundsPlayed === selectedGameRound) {
+    if (playerScore > computerScore) {
+      gameOverMessage_h3.textContent = `Game Over! You won the game.`;
+    } else if (computerScore > playerScore) {
+      gameOverMessage_h3.textContent =  `Game Over! You won the game.`;
+    } else {
+      gameOverMessage_h3.textContent = `Game over! It's a tie! `;
+    }
+  };
 };
 
-
+//play game with button
 function main() {
   rock_div.addEventListener("click", () => {
     game("rock");
